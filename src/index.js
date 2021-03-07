d59c6992990a7a08e5a19c70ec2471f11c1cc89a
evgeny <bakalov.es@ya.ru>
2021-03-07 11:40:21 +0300

    feat: search min, max and avg values

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+exports.min = function min(array) {
+    if (typeof array !== 'undefined' && array.length > 0) {
+        return Math.min.apply(null, array);
+    }
 
-exports.min = function min (array) {
-  return 0;
+    return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (typeof array !== 'undefined' && array.length > 0) {
+        return Math.max.apply(null, array);
+    }
+
+    return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (typeof array !== 'undefined' && array.length > 0) {
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum += parseInt(array[i], 10);
+        }
+
+        return sum / array.length;
+    }
+
+    return 0;
 }
